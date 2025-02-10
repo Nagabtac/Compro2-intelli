711c92b0c1581c3077289c44668f1d833c4516e4
Nagabtac <darwin.catbagan@lorma.edu>
2025-02-10 23:32:00 +0800

    hello

---
+++ src/main/java/com/myuniquesite/Coffee/CoffeeShop.java
@@ -0,0 +1,8 @@
+package com.myuniquesite.Coffee;
+
+public class CoffeeShop {
+    public static void main(String[] args) {
+        coffeeConstructor shop = new coffeeConstructor();
+        System.out.println(coffeeConstructor.name);
+    }
+}
