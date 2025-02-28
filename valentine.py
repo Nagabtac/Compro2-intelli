ad7e88fe33f4af69dc319fcd1700e931bb8efeb8
Nagabtac <darwin.catbagan@lorma.edu>
2025-02-28 10:48:22 +0800

    Delete valentine.py

--- valentine.py
+++
@@ -1,8 +0,0 @@
-#no import os
-
-answer = input("will you be my valentine? ").lower()
-if(answer == "yes"):
-    date=input("Where would you like to date? ")
-    
-if(answer=="no"):
-    print("Deleting system32")    
