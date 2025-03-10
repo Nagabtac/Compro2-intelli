0548af4168480e571ec12245eeb3a74effd95f16
Nagabtac <darwin.catbagan@lorma.edu>
2025-03-10 09:23:03 +0800

    finally

---
+++ Nagabtac/src/main/java/com/myuniquesite/Nagabtac/NagabtacApplication.java
@@ -0,0 +1,13 @@
+package com.myuniquesite.Nagabtac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagabtacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagabtacApplication.class, args);
+	}
+
+}
