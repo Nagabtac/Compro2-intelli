3f0db2f40794e227c6c1d65889e627ea74490e92
Nagabtac <darwin.catbagan@lorma.edu>
2025-03-13 16:02:56 +0800

    d

--- activitymar7.py
+++ activitymar7.py
@@ -1,15 +1,15 @@
 def hello():
     queue = []
-    queue.append(3)#adds "Hello" to the queue
+    queue.append(3)
     queue.append(5)
     queue.append(9)
     queue.append(12)
     queue.append(15)
     
+    print(queue)# prints queue
+    #Dequeues index[0] or 3
     
-    queue.pop(0)#Dequeues index[0] or 3
-    
-    print("element removed: ",queue[0])
+    print("element removed: ",queue.pop(0))
     
     print("updated queue ", queue)#Prints the queue list
 
@@ -22,6 +22,7 @@ if __name__ == "__main__":
 #1 Start
 #2.initialize a queue
 #3. enqueue elements 3, 5,9,12,15
-#4.dequeue index[3] or 3
-#5.print updated queue
-#6.End
+#4. print queue
+#5.dequeue index[0] or 3
+#6.print updated queue
+#7.End
